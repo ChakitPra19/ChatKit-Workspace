f34e99e5f8f6608d06973f97be6aaa796d47cb23
ChakitPra19 <chakitpra19@gmail.com>
2026-05-29 22:41:18 +0700

    feat: add .gitignore file and configure bun development script in package.json

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import express from "express"; // Framework ช่วยให้สร้าง api ง่ายขึ้น
+import cors from "cors"; // ใช้เพื่อให้ Frontend เข้าถึง Backend ได้
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Server Available!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
